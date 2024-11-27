8d3efce77c99661608a91e6fff646b86ade38a4b
Tymon Jasiński <tymon.jasinski010@gmail.com>
2024-11-28 00:32:36 +0100

    lab1 fixes

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-
+const searchBar = document.getElementById("searchBar")
+const list = document.getElementById("pokemonList")
+const detailContainer = document.getElementById("detailContainer")
+const imageBox = document.getElementById("pokemonImageBox")
 
 const getPokemonList = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
-    const json = await res.json();
-
-    const list = document.getElementById("list")
-    json.results.forEach((el) => {
-        const li = document.createElement("li")
-        li.textContent = el.name
-        list.appendChild(li)
-
-    })
+    list.innerHTML="Loading..."
+    try{
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
+        const json = await res.json();
+        showPokemonList(json.results)
+    }
+    catch (error){
+        list.innerHTML="Failed to load the Pokemons"
+    }
     
 }
+const showPokemonList = async (pokemons) => {
+    list.innerHTML=""
+    pokemons.forEach((el,i) => { 
+        const pokemon = document.createElement("div")
+        pokemon.classList.add("pokemonCard");
+        
+        const id = i+1
+           
+        pokemon.innerHTML=`<p>${el.name} #${id}</p><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">`
+        pokemon.addEventListener("click", () => getPokemonDetails(id))
+        list.appendChild(pokemon)
+    })
+}
+window.onload = getPokemonList
 
 const getPokemonDetails = async (id) => {
-    const res = await fetch(`https://pokeapi.co/api/v2/characteristic/${id}/`)
-    const json = await res.json()
-    console.log(json.results);
-    
-
-    
+    imageBox.innerHTML="Loading..."
+    try{
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        const json = await res.json()
+        showPokemonDetails(json)
+    }
+    catch (error){
+        detailContainer.innerHTML="Loading the Pokemon's details has failed"
+    }
 }
 
-window.onload = getPokemonList()
+const showPokemonDetails = async (pokemon) => {
+    imageBox.innerHTML=""
+    imageBox.innerHTML = `<p>${pokemon.name} #${pokemon.id}</p><img src="${pokemon.sprites.front_default}">`
+    const detailBox = document.getElementById("pokemonDetailBox");
+    detailBox.innerHTML = 
+    `<p><strong>Types: </strong>${pokemon.types.map(el => el.type.name).join(", ")}</p> 
+    <p><strong>Height: </strong>${pokemon.height}</p>
+    <p><strong>Weight: </strong>${pokemon.weight}</p>
+    ${pokemon.stats.map(stat => `<p><strong>${stat.stat.name}: </strong>${stat.base_stat}</p>`).join('')}`
+}
 
-const button = document.getElementById("search")
-button.addEventListener("click",getPokemonDetails)
+// const button = document.getElementById("search")
+// button.addEventListener("click",getPokemonDetails)
+
